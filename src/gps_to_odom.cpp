d9a77b2c9ae4e30ea5e4b6e498b5ab6437dd2912
Marco Scianna <marco.scianna@mail.polimi.it>
2024-04-09 11:50:50 +0200

    bau

--- src/gps_to_odom.cpp
+++ src/gps_to_odom.cpp
@@ -2,6 +2,7 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Point.h"
 #include <std_msgs/String.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <cmath>
 
 // Variabili globali per memorizzare i valori dei parametri
@@ -11,67 +12,61 @@ double alt_zero;
 
 // Funzione per convertire le coordinate GPS in coordinate ECEF
 void GpsToEcef(double lat, double lon, double alt, double& ecef_x, double& ecef_y, double& ecef_z){
-    double e = 0.00669437999014;
-    double a = 6378137;
+    double e = 0.08181919; // valore di e approssimato per il WGS84 ellipsoid
+    double a = 6378137;    // raggio equatoriale del WGS84 ellipsoid
     double N;
 
-    N=a/sqrt(1-(e*e*sin(lat)*sin(lat)));
-    ecef_x=(N+alt)*cos(lat)*cos(lon);
-    ecef_y=(N+alt)*cos(lat)*sin(lon);
-    ecef_z=((1-e*e)*N+alt)*sin(lat);
+    N = a / sqrt(1 - (e * e * sin(lat) * sin(lat)));
+    ecef_x = (N + alt) * cos(lat) * cos(lon);
+    ecef_y = (N + alt) * cos(lat) * sin(lon);
+    ecef_z = ((1 - e * e) * N + alt) * sin(lat);
 }
 
-// Funzione per convertire le coordinate ECEF in coordinate NED rispetto all'origine (0,0,0)
-void ecefToNED(double ecef_x, double ecef_y, double ecef_z, double& ned_x, double& ned_y, double& ned_z) {
-   // Calcola le differenze di posizione rispetto all'origine (0,0,0) in coordinate ECEF
-   ned_x = ecef_x;
-   ned_y = ecef_y;
-   ned_z = ecef_z;
-}
+void callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+    // Estrai i dati di odometria dal messaggio
+    double latitude = msg->latitude -lat_zero;
+    double longitude = msg->longitude -lon_zero;
+    double altitude = msg->altitude - alt_zero;
 
-void Callback(const nav_msgs::Odometry::ConstPtr& msg) {
-   // Estrai i dati di odometria dal messaggio
-   double x = msg->pose.pose.position.x;
-   double y = msg->pose.pose.position.y;
-   double z = msg->pose.pose.position.z;
+    // Stampa i dati di odometria
+    //ROS_INFO("Received GPS data: latitude=%f, longitude=%f, altitude=%f", latitude, longitude, altitude);
 
-   // Stampa i dati di odometria
-   ROS_INFO("Received odometry data: x=%f, y=%f, z=%f", x, y, z);
-//Calcola le coordinate ECEF
+    // Calcola le coordinate ECEF della posizione GPS ricevuta
     double ecef_x, ecef_y, ecef_z;
-    GpsToEcef(x, y, z, ecef_x, ecef_y, ecef_z);
+    GpsToEcef(latitude, longitude, altitude, ecef_x, ecef_y, ecef_z);
 
-   // Calcola le coordinate NED rispetto all'origine (0,0,0)
-   double ned_x, ned_y, ned_z;
-   ecefToNED(ecef_x, ecef_y, ecef_z, ned_x, ned_y, ned_z);
+    // Calcola le coordinate ECEF dell'origine
+    double origin_ecef_x, origin_ecef_y, origin_ecef_z;
+    
 
-   // Aggiungi le coordinate di riferimento
-   ned_x -= lon_zero;
-   ned_y -= lat_zero;
-   ned_z -= alt_zero;
+    // Calcola le coordinate NED rispetto all'origine (0,0,0)
+    double ned_x = ecef_x ;
+    double ned_y = ecef_y; 
+    double ned_z = ecef_z; 
 
-   // Crea un nuovo messaggio di odometria per inviare i dati elaborati
-   nav_msgs::Odometry output_msg;
-   output_msg.header = msg->header;
-   output_msg.pose.pose.position.x = ned_x; // Coordinate NED rispetto all'origine (0,0,0)
-   output_msg.pose.pose.position.y = ned_y;
-   output_msg.pose.pose.position.z = ned_z;
+   //ROS_INFO("Received NED data: latitude=%f, longitude=%f, altitude=%f", ned_x, ned_y, ned_z);
+    // Crea un nuovo messaggio di odometria per inviare i dati elaborati
+    nav_msgs::Odometry output_msg;
+    output_msg.header = msg->header;
+    output_msg.pose.pose.position.x = ned_x; // Coordinate NED rispetto all'origine (0,0,0)
+    output_msg.pose.pose.position.y = ned_y;
+    output_msg.pose.pose.position.z = ned_z;
 
-   // Pubblica il messaggio di odometria elaborato sul topic input_odom
-   ros::NodeHandle n;
-   ros::Publisher pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
-   pub.publish(output_msg);
+    // Pubblica il messaggio di odometria elaborato sul topic "/gps_odom"
+    ros::NodeHandle n;
+    ros::Publisher pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
+    pub.publish(output_msg);
 }
 
 int main(int argc, char **argv){
-   ros::init(argc, argv, "talker");
-   ros::NodeHandle n;
-   // Leggi i parametri lat_zero, lon_zero, e alt_zero
-   n.param("lat_zero", lat_zero, 0.0);
-   n.param("lon_zero", lon_zero, 0.0);
-   n.param("alt_zero", alt_zero, 0.0);
-   // Crea un subscriber che ascolta i messaggi dal topic "/gps_odom"
-   ros::Subscriber sub = n.subscribe("/fix", 1, Callback);
-   ros::spin(); // Mantieni il nodo in esecuzione finché non viene terminato esternamente
-   return 0;
+    ros::init(argc, argv, "gps_to_odom_converter");
+    ros::NodeHandle n;
+    // Leggi i parametri lat_zero, lon_zero, e alt_zero
+    n.param("lat_zero", lat_zero, 0.0);
+    n.param("lon_zero", lon_zero, 0.0);
+    n.param("alt_zero", alt_zero, 0.0);
+    // Crea un subscriber che ascolta i messaggi dal topic "/fix"
+    ros::Subscriber sub = n.subscribe("/fix", 1, callback);
+    ros::spin(); // Mantieni il nodo in esecuzione finché non viene terminato esternamente
+    return 0;
 }
--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -1,15 +1,22 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
 
 class OdomToTFNode {
 public:
     OdomToTFNode() {
-        ros::NodeHandle nh("~");
+        ros::NodeHandle nh;
+        ros::NodeHandle private_nh("~");
 
         // Retrieve node parameters
-        nh.param<std::string>("root_frame", root_frame_, "world");
-        nh.param<std::string>("child_frame", child_frame_, "world");
+        //string root_frame;
+        //string child_frame;
+        nh.param<std::string>("root_frame", root_frame, "world");
+        nh.param<std::string>("child_frame", child_frame, "wheel_odom");
+
+        ROS_INFO("Root frame: %s, Child frame: %s", root_frame.c_str(), child_frame.c_str());
 
         // Subscribe to the input odometry topic
         odom_sub_ = nh.subscribe("input_odom", 1, &OdomToTFNode::odomCallback, this);
@@ -17,13 +24,23 @@ public:
 
     void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
         // Convert odometry message to tf message
+        //ROS_INFO("Messaggio ricevuto: %s", msg->header.frame_id.c_str());
+
         geometry_msgs::TransformStamped transformStamped;
+
+        transformStamped.header.stamp = ros::Time::now();
         transformStamped.header = msg->header;
-        transformStamped.child_frame_id = child_frame_;
+        transformStamped.child_frame_id = child_frame;
         transformStamped.transform.translation.x = msg->pose.pose.position.x;
         transformStamped.transform.translation.y = msg->pose.pose.position.y;
         transformStamped.transform.translation.z = msg->pose.pose.position.z;
-        transformStamped.transform.rotation = msg->pose.pose.orientation;
+
+        tf2::Quaternion quaternion;
+        quaternion.setRPY(0, 0, msg->pose.pose.orientation.z);
+        transformStamped.transform.rotation.x = quaternion.x();
+        transformStamped.transform.rotation.y = quaternion.y();
+        transformStamped.transform.rotation.z = quaternion.z();
+        transformStamped.transform.rotation.w = quaternion.w();
 
         // Publish the transform
         tf_broadcaster_.sendTransform(transformStamped);
@@ -32,8 +49,8 @@ public:
 private:
     ros::Subscriber odom_sub_;
     tf::TransformBroadcaster tf_broadcaster_;
-    std::string root_frame_;
-    std::string child_frame_;
+    std::string root_frame;
+    std::string child_frame;
 };
 
 int main(int argc, char** argv) {
