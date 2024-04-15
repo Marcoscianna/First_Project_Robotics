66aca45322c58a4a29416a284f6c41b53cd1a759
Marco Scianna <marco.scianna@mail.polimi.it>
2024-04-15 13:12:57 +0200

    test

--- src/lidar_visualization.cpp
+++ src/lidar_visualization.cpp
@@ -1,22 +1,46 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <dynamic_reconfigure/server.h>
 #include <first_project/parametersConfig.h>
 
 class LidarVisualizationNode {
-
 private:
     ros::NodeHandle nh_;
     ros::Subscriber sub_;
+    ros::Publisher pub_;
     std::string lidar_frame_;
 
-    // Callback per il messaggio LaserScan
-    void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-        // Qui puoi gestire i dati del messaggio LaserScan
-        // Ad esempio, puoi modificare il frame_id e pubblicare il messaggio modificato
-        sensor_msgs::LaserScan modified_scan = *msg;
-        modified_scan.header.frame_id = lidar_frame_;
-        // Qui puoi pubblicare il messaggio modificato o eseguire altre operazioni necessarie
+    // Callback per il messaggio PointCloud2
+    void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+        // Qui puoi convertire i dati della nuvola di punti in dati di scansione laser
+
+        // Supponiamo che tu voglia convertire la distanza media dei punti in una scansione laser
+        sensor_msgs::LaserScan laser_scan;
+        laser_scan.header = msg->header;
+        laser_scan.angle_min = -M_PI / 2;  // Angolo minimo
+        laser_scan.angle_max = M_PI / 2;   // Angolo massimo
+        laser_scan.angle_increment = M_PI / msg->width;  // Incremento dell'angolo
+        laser_scan.time_increment = 0.0;  // Incremento del tempo
+        laser_scan.scan_time = 0.1;  // Tempo di scansione
+        laser_scan.range_min = 0.0;  // Distanza minima rilevabile
+        laser_scan.range_max = 10.0;  // Distanza massima rilevabile
+
+        // Calcola la distanza media dei punti e assegnala alla scansione laser
+        std::vector<float> ranges;
+        for (size_t i = 0; i < msg->width; ++i) {
+            float range = sqrt(pow(msg->data[i * msg->point_step], 2) + 
+                               pow(msg->data[i * msg->point_step + 1], 2) + 
+                               pow(msg->data[i * msg->point_step + 2], 2));
+            ranges.push_back(range);
+        }
+        laser_scan.ranges = ranges;
+
+        // Imposta il frame ID
+        laser_scan.header.frame_id = lidar_frame_;
+
+        // Pubblica il messaggio di scansione laser
+        pub_.publish(laser_scan);
     }
 
     // Callback per la riconfigurazione dinamica
@@ -43,6 +67,9 @@ public:
 
         // Inizializzazione del frame_id predefinito
         lidar_frame_ = "default_frame"; 
+
+        // Inizializzazione del publisher per pubblicare i dati del sensore laser modificati
+        pub_ = nh_.advertise<sensor_msgs::LaserScan>("/modified_scan", 1000);
     }
 
     void run() {
@@ -54,7 +81,7 @@ public:
         server.setCallback(f);
 
         // Sottoscrizione al topic "/os_cloud_node/points"
-        sub_ = nh_.subscribe("/os_cloud_node/points",1000, &LidarVisualizationNode::laserScanCallback, this);
+        sub_ = nh_.subscribe("/os_cloud_node/points", 1000, &LidarVisualizationNode::pointCloudCallback, this);
 
         ROS_INFO("Nodo avviato");
         ros::spin();
--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -1,32 +1,25 @@
 #include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_broadcaster.h>
 
 class OdomToTFNode {
-public:
-    OdomToTFNode(ros::NodeHandle& nh) {
-        // Retrieve node parameters
-        nh.param<std::string>("root_frame", root_frame, "world");
-        nh.param<std::string>("child_frame", child_frame, "default");
-
-        //ROS_INFO("Root frame: %s, Child frame: %s", root_frame.c_str(), child_frame.c_str());
-
-        // Subscribe to the input odometry topic
-        odom_sub_ = nh.subscribe("/input_odom", 1000, &OdomToTFNode::odomCallback, this);
-    }
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber sub_;
+    tf::TransformBroadcaster tf_broadcaster_;
+    std::string root_frame_;
+    std::string child_frame_;
 
     void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
-        // Convert odometry message to tf message
+        // Calcola la trasformazione e pubblicala utilizzando il broadcaster tf
         geometry_msgs::TransformStamped transformStamped;
         transformStamped.header.stamp = ros::Time::now();
-        transformStamped.header = msg->header;
-        transformStamped.child_frame_id = child_frame;
-        transformStamped.header.frame_id = root_frame;
+        transformStamped.header.frame_id = root_frame_;
+        transformStamped.child_frame_id = child_frame_;
         transformStamped.transform.translation.x = msg->pose.pose.position.x;
         transformStamped.transform.translation.y = msg->pose.pose.position.y;
         transformStamped.transform.translation.z = msg->pose.pose.position.z;
+        transformStamped.transform.rotation = msg->pose.pose.orientation;
 
         tf2::Quaternion quaternion;
         quaternion.setRPY(0, 0, msg->pose.pose.orientation.z);
@@ -35,29 +28,27 @@ public:
         transformStamped.transform.rotation.z = quaternion.z();
         transformStamped.transform.rotation.w = quaternion.w();
 
-        // Publish the transform
         tf_broadcaster_.sendTransform(transformStamped);
-        
-            //ROS_INFO("Translation: x=%f, y=%f, z=%f",
-              //transformStamped.transform.translation.x,
-              //transformStamped.transform.translation.y,
-              //transformStamped.transform.translation.z);
-
     }
 
-private:
-    ros::Subscriber odom_sub_;
-    tf::TransformBroadcaster tf_broadcaster_;
-    std::string root_frame;
-    std::string child_frame;
+public:
+    OdomToTFNode() {
+        nh_ = ros::NodeHandle("~");
+        nh_.getParam("root_frame", root_frame_);
+        nh_.getParam("child_frame", child_frame_);
+        sub_ = nh_.subscribe("/odom", 1000, &OdomToTFNode::odomCallback, this);
+    }
+
+    void run() {
+        ros::spin();
+    }
 };
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "odom_to_tf_node");
-        
-    ros::NodeHandle nh("~");
-    OdomToTFNode odom_to_tf_gps(nh);
+    ros::init(argc, argv, "odom_to_tf");
 
-    ros::spin();
+    OdomToTFNode node;
+    node.run();
+
     return 0;
 }
